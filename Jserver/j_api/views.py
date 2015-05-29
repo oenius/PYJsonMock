3b22e7ba4517a0c59f76995c14a2b329b7bdb4c0
monfur.yuri <monfur.yuri.jou@gmail.com>
2015-05-29 14:47:58 +0800

    [MOD] exception error msg

--- Jserver/j_api/views.py
+++ Jserver/j_api/views.py
@@ -8,12 +8,27 @@ import json
 
 
 def resolve_api(request):
+	url = request.path
+	short_name = url.split("/")[-1]
+	file_name = settings.JSON_PATH + url.split("/")[-1]
 	try:
-		url = request.path
-		file_name = settings.JSON_PATH + url.split("/")[-1]
-	
-		json_file= open(file_name)
-		json_data = json.load(json_file)
+		try:
+			json_file= open(file_name)
+		except Exception, e:
+			response_data = {
+			'code':'-1',
+			'msg':'JSON FILE ERROR: ' + short_name + ' file can not open, make sure it is exit',
+			}	
+			return HttpResponse(json.dumps(response_data), content_type="application/json")	
+		try:
+			json_data = json.load(json_file)
+		except Exception, e:
+			response_data = {
+			'code':'-1',
+			'msg':'JSON FORMATE ERROR: ' + short_name + ' file is exit,but you should check your json file',
+			}	
+			return HttpResponse(json.dumps(response_data), content_type="application/json")	
+		
 		return HttpResponse(json.dumps(json_data), content_type="application/json")	
 	except Exception, e:
 		response_data = {
